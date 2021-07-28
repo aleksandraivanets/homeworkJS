1885c879a8fcf72d44366057f310fd5eb2847018
aleksandraivanets <aleksandraivanets@gmail.com>
2021-07-28 09:12:57 +0300

    added hm4

--- module 4/js/task-2.js
+++ module 4/js/task-2.js
@@ -8,24 +8,24 @@ const inventory = {
     this.items.push(itemName);
   },
   remove(itemName) {
-    console.log(`Removing ${this.itemName} from inventory`);
+    console.log(`Removing ${itemName} from inventory`);
 
-    items = this.items.filter((item) => item !== itemName);
+    this.items = this.items.filter((item) => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function (itemName, action) {
-  console.log("Invoking action on :", itemName);
+  console.log(`Invoking action on ${itemName}`);
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
